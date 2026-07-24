1080a10314f7580f71ad6fc1702617ca21cad568
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2026-07-24 17:24:36 -0400

    updated types

--- internal/unitops/types.go
+++ internal/unitops/types.go
@@ -2,55 +2,43 @@
 package unitops
 
 type TypeDefinition interface {
-	GetTypeName() string
-	GetCodeComments() string
+	GetPrompt() string
 	GetDependentTypes() []*TypeDefinition
-	GetFilePath() string
-	GetShouldStub() bool
 }
 
 type UnitOperation interface {
-	GetUnitOperationTypeDefinition() string
 	GetInputTypes() []*TypeDefinition
 	GetOutputTypes() []*TypeDefinition
 	GetPrompt() string
-	GetCodeComments() string
-	GetDocumentation() string
 	GetFunctionCalls() []*FunctionDefinition
-	GetShouldStub() bool
 }
 
 type FunctionDefinition interface {
 	GetFunctionName() string
-	GetCodeComments() string
+	GetPrompt() string
 	GetUnitOperations() []*UnitOperation
-	GetShouldStub() bool
 }
 
 type TestCase interface {
 	GetName() string
-	GetDefinitionPrompt() string
-	GetCodeComments() string
-	GetShouldStub() bool
+	GetPrompt() string
 }
 
 type TestSuite interface {
-	GetCodeComments() string
+	GetPrompt() string
 	GetFunctionDefinition() *FunctionDefinition
 	GetTestCases() []*TestCase
 }
 
 type TestFileDefinition interface {
-	FileDefinitionPrompt() string
+	GetPrompt() string
 	GetFilePath() string
-	GetCodeComments() string
 	GetTestSuites() []*TestSuite
 }
 
 type ProductionFileDefinition interface {
-	FileDefinitionPrompt() string
+	GetPrompt() string
 	GetFilePath() string
-	GetCodeComments() string
 	GetTypeDefinitions() []*TypeDefinition
 	GetFunctionDefinitions() []*FunctionDefinition
 }
