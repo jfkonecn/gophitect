9dca6ee34b918c9a274875959e1f7ee00a82f7af
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2026-07-24 09:12:49 -0400

    Comments

--- internal/unitops/types.go
+++ internal/unitops/types.go
@@ -6,6 +6,7 @@ type TypeDefinition interface {
 	GetCodeComments() string
 	GetDependentTypes() []TypeDefinition
 	GetFilePath() string
+	GetShouldStub() bool
 }
 
 type UnitOperation interface {
@@ -16,12 +17,14 @@ type UnitOperation interface {
 	GetCodeComments() string
 	GetDocumentation() string
 	GetFunctionCalls() []FunctionDefinition
+	GetShouldStub() bool
 }
 
 type FunctionDefinition interface {
 	GetFunctionName()
 	GetCodeComments() string
 	GetUnitOperations() []UnitOperation
+	GetShouldStub() bool
 }
 
 type FileDefinition interface {
@@ -32,35 +35,46 @@ type FileDefinition interface {
 	GetFunctionDefinitions() []FunctionDefinition
 }
 
+// A Map unit operation converts one value or type into another.
 type Map struct {
 }
 
+// A Filter unit operation removes, rejects, or reroutes data.
 type Filter struct {
 }
 
+// A Sort unit operation orders a collection according to defined rules.
 type Sort struct {
 }
 
+// A Distribution unit operation chooses where data goes next, such as branching.
 type Distribution struct {
 }
 
+// A Validate unit operation checks data integrity.
 type Validate struct {
 }
 
+// An Authenticate unit operation determines the identity initiating a flow.
 type Authenticate struct {
 }
 
+// An Authorize unit operation determines the whether an authenticated identity may perform an action.
 type Authorize struct {
 }
 
+// A GlobalStateRead unit operation reads values whose lifetime extends beyond the current call stack
 type GlobalStateRead struct {
 }
 
+// A GlobalStateWrite unit operation writes values whose lifetime extends beyond the current call stack
 type GlobalStateWrite struct {
 }
 
+// An InputOutput unit operation communicates outside the program, including HTTP, databases, etc...
 type InputOutput struct {
 }
 
+// A Panic unit operation terminates execution of the program.
 type Panic struct {
 }
